66750e649a7aa394b6c46928eb37843ca5f8fb5e
David <davidmh.mail@gmail.com>
2019-12-15 16:36:02 -0700

    Implement stopMonitor handler to allow client to stop the monitor

---
+++ src/projects/projects.constants.ts
@@ -0,0 +1,3 @@
+export const ProjectsConstants = {
+  minMonitorInterval: 5000,
+};
--- src/projects/projects.gateway.ts
+++ src/projects/projects.gateway.ts
@@ -13,6 +13,7 @@ import { ProjectsService } from './projects.service';
 import ProjectsMonitorConfig from './interfaces/projectsMonitorConfig.interface';
 import { Observable, of, Subject } from 'rxjs';
 import { catchError, map, takeUntil, tap } from 'rxjs/operators';
+import { ProjectsConstants } from './projects.constants';
 
 @WebSocketGateway(5005, { namespace: 'projects-monitor' })
 export class ProjectsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
@@ -38,7 +39,7 @@ export class ProjectsGateway implements OnGatewayInit, OnGatewayConnection, OnGa
   }
 
   @SubscribeMessage('msgToServer')
-  handleMessage(@MessageBody() projectConfigs: string): Observable<WsResponse<any>> {
+  handleProjectsConfigMsg(@MessageBody() projectConfigs: string): Observable<WsResponse<any>> {
     this.logger.log('received msgToServer\n' + projectConfigs);
     const parsedProjectConfigs: ProjectsMonitorConfig = JSON.parse(projectConfigs);
     this.logger.debug('received msgToServer\n' + JSON.stringify(parsedProjectConfigs.projects[0]));
@@ -48,9 +49,17 @@ export class ProjectsGateway implements OnGatewayInit, OnGatewayConnection, OnGa
     );
   }
 
+  @SubscribeMessage('msgToServer:stopMonitor')
+  handleStopMonitorMsg(): WsResponse<string> {
+    const data = 'Projects monitor stopped';
+    this.unsubscribe$.next(true);
+    this.logger.log('msgToClient:stopMonitor sent');
+    return {event: 'msgToClient:stopMonitor', data};
+  }
+
   monitorProjects(projects: any, intervalLength: number): Observable<unknown> {
     this.logger.debug(`Monitoring projects for client every ${intervalLength} milliseconds.`);
-    const minIntervalLength = 5000;
+    const minIntervalLength = ProjectsConstants.minMonitorInterval;
     // TODO - Make minimum intervalLength constant based on env (shorter for dev, longer for prod)
     if (intervalLength && intervalLength >= minIntervalLength) {
       return this.projectsService.monitorProjects(projects, intervalLength).pipe(
